388994cdc3a0c002be7a665f64609e4b24261f92
Asylum <asxlvm@asxlvm.asxlvm.me>
2022-06-07 15:27:28 +0200

    Added home template with css

--- SerenityProject/settings.py
+++ SerenityProject/settings.py
@@ -116,7 +116,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/SerenityForum/'
+STATIC_URL = 'static/'
+STATIC_ROOT = "../SerenityForum/static/SerenityForum/"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
