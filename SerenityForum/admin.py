c920d7361c0f74117fd98eb556047451ad562965
Asylum <asxlvm@asxlvm.asxlvm.me>
2022-06-07 17:46:09 +0200

    Edited models, added them into admin panel, changed navbar

--- SerenityForum/admin.py
+++ SerenityForum/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import User, UserRole, Post, Comment, Download, Category
+
+for Model in [User, UserRole, Post, Comment, Download, Category]:
+    admin.site.register(Model)
--- SerenityForum/models.py
+++ SerenityForum/models.py
@@ -6,11 +6,11 @@ class UserRole(models.Model):
     name = models.CharField(max_length=20)
     color = models.CharField(default="#333333", max_length=9)
     numberofusers = models.IntegerField(default=0)
-    def __str__():
+    def __str__(self):
         return self.name
 
 class Category(models.Model):
-    posts = models.ForeignKey('Post', null = False, on_delete=models.CASCADE)
+    posts = models.ForeignKey('Post', null = True, on_delete=models.CASCADE)
     numberofposts = models.IntegerField(default=0)
     title = models.CharField(max_length=100, default="A category")
     description = models.CharField(max_length=100, default="The description of a category.")
@@ -18,7 +18,7 @@ class Category(models.Model):
 
 class Download(models.Model):
     downloadlink = models.URLField(max_length=100)
-    def __str__():
+    def __str__(self):
         return self.downloadlink
 
 class Comment(models.Model):
@@ -29,10 +29,10 @@ class Comment(models.Model):
 class Post(models.Model):
     author = models.ForeignKey('User', null = False, on_delete=models.CASCADE)
     postid = models.UUIDField(default=uuid.uuid4, unique=True, editable=False)
-    comments = models.ForeignKey('Comment', null = False, on_delete=models.CASCADE)
+    comments = models.ForeignKey('Comment', null = True, on_delete=models.CASCADE)
     description = models.CharField(max_length=1000)
     title = models.CharField(max_length=75)
-    downloads = models.ForeignKey('Download', null = False, on_delete=models.CASCADE)
+    downloads = models.ForeignKey('Download', null = True, on_delete=models.CASCADE)
     postcategory = models.ForeignKey('Category', null = False, on_delete=models.CASCADE)
     upvotes = models.IntegerField(default=0)
     createdat = models.BigIntegerField(default=time.time)
@@ -43,9 +43,11 @@ class User(models.Model):
     userbio = models.CharField(max_length=500, default="This user has not modified his bio.")
     userupvotes = models.IntegerField(default=0)
     userpfp = models.URLField(max_length=200, default="https://i.pinimg.com/736x/c9/e3/e8/c9e3e810a8066b885ca4e882460785fa.jpg")
-    userposts = models.ForeignKey('Post', null = False, on_delete=models.CASCADE)
+    userposts = models.ForeignKey('Post', null = True, on_delete=models.CASCADE)
     password = models.CharField(max_length=200)
     token = models.CharField(max_length=100)
     createdat = models.BigIntegerField(default=time.time)
     role = models.ForeignKey('UserRole', null = False, on_delete=models.CASCADE)
     banned = models.BooleanField(default=False)
+    ip = models.GenericIPAddressField(default="255.255.255.255")
+    email = models.EmailField(default="your@email.com")
