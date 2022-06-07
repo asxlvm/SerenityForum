a4a0dff9889763544effbfccc7773a0cce86a5a0
Asylum <asxlvm@asxlvm.asxlvm.me>
2022-06-07 21:46:25 +0200

    Added category page & home page with categories

--- SerenityForum/views.py
+++ SerenityForum/views.py
@@ -1,5 +1,20 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.template import loader
+from .models import User, UserRole, Post, Comment, Download, Category
 
 def home(request):
-    return render(request, "SerenityForum/home.html")
+    categories = Category.objects.all()
+    template = loader.get_template("SerenityForum/home.html")
+    ctx = {
+        "categories": categories,
+    }
+    return HttpResponse(template.render(ctx, request))
+
+def categoryview(request, categoryname):
+    category = Category.objects.filter(title=categoryname)[0]
+    template = loader.get_template("SerenityForum/categories.html")
+    ctx = {
+        "category": category,
+        "posts": Post.objects.filter(postcategory=category)
+    }
+    return HttpResponse(template.render(ctx, request))
