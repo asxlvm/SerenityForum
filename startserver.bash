f2dad2b19592b32a83f21017f0f1fa642e5e180c
Nikolas Peterka <81298611+asxlvm@users.noreply.github.com>
2022-10-22 16:20:58 +0200

    Update startserver.bash

--- startserver.bash
+++ startserver.bash
@@ -4,4 +4,4 @@ export MONGOURI="mongodb+srv://asxlvm:A6wRgGs5INzVXdYw@cluster0.nm2p7.mongodb.ne
 
 echo "[INFO] Exported environment variables, starting ($(TZ='Europe/Prague' date) - $(TZ='Europe/Prague' date +%s))"
 
-python3 manage.py runserver $PORT
+python3 manage.py runserver
