24ef6526ca7db5e468b95724725047256f344d62
Nikolas Peterka <81298611+asxlvm@users.noreply.github.com>
2022-10-22 16:29:53 +0200

    Update settings.py

--- SerenityProject/settings.py
+++ SerenityProject/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 import django_heroku
 
-django_heroku.settings(locals())
+
 
 from pathlib import Path
 
@@ -134,3 +134,5 @@ STATIC_ROOT = "../SerenityForum/static/SerenityForum/"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
