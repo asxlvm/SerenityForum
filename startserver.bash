3cb899ecd48b2e03da736b54672948ded688dafb
Nikolas Peterka <81298611+asxlvm@users.noreply.github.com>
2022-10-22 16:11:05 +0200

    Update startserver.bash

--- startserver.bash
+++ startserver.bash
@@ -2,11 +2,7 @@
 
 export MONGOURI="mongodb+srv://asxlvm:A6wRgGs5INzVXdYw@cluster0.nm2p7.mongodb.net"
 
-if [ -z $1 ] then
- export PORT="8080"
-else
- export PORT=$1
-fi
+export PORT=$1
 
 echo "[INFO] Exported environment variables, starting ($(TZ='Europe/Prague' date) - $(TZ='Europe/Prague' date +%s))"
 
